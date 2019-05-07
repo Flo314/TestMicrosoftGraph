7035c06435744fc5e81f08ffc089dc1c36d9932d
“Florent <f.losada@hotmail.fr>
2019-05-07 10:29:30 +0200

    suppression bouton signout

--- app/src/main/java/com/dtc/testauthgraph/LoginActivity.java
+++ app/src/main/java/com/dtc/testauthgraph/LoginActivity.java
@@ -6,8 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -37,15 +35,6 @@ public class LoginActivity extends AppCompatActivity implements MSALAuthenticati
                 onSignin();
             }
         });
-
-//        // button de déconnection qui rend la progressBar invisible quand on clique sur le button via la méthode onSignin
-//        Button btnsignout = findViewById(R.id.btnsignout);
-//        btnsignout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                onSignout();
-//            }
-//        });
 
     }
 
