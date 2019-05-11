e3395290c3884a3ce2a4c0bdfb2155a6255aea4c
“Florent <f.losada@hotmail.fr>
2019-05-11 16:28:11 +0200

    toolbar dans menu activity

--- app/src/main/java/com/dtc/testauthgraph/screen/MenuApp.java
+++ app/src/main/java/com/dtc/testauthgraph/screen/MenuApp.java
@@ -1,12 +1,11 @@
 package com.dtc.testauthgraph.screen;
 
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.dtc.testauthgraph.R;
-import com.dtc.testauthgraph.ToolbarFragment;
+
 
 
 
@@ -19,13 +18,11 @@ public class MenuApp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_app);
 
-//        // on instancie le fragment manager
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        // on instancie la transaction grâce au fragment manager
-//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//        ToolbarFragment toolbarFragment = new ToolbarFragment();
-//        // on remplace le contenu actuel par le fragment 1
-//        fragmentTransaction.replace(android.R.id.content,toolbarFragment);
-//        fragmentTransaction.commit();
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.btn_bg_load));
+            actionBar.setTitle("Dashboard");
+        }
+
     }
 }
