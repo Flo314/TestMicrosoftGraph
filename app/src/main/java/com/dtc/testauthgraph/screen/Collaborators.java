b2ea92390d9aece19dbbef49fa2eed530f72636f
“Florent <f.losada@hotmail.fr>
2019-05-11 17:13:29 +0200

    ajout actvity collaborators

--- app/src/main/java/com/dtc/testauthgraph/screen/Collaborators.java
+++ app/src/main/java/com/dtc/testauthgraph/screen/Collaborators.java
@@ -1,35 +1,22 @@
 package com.dtc.testauthgraph.screen;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.dtc.testauthgraph.R;
 
-
-
-
-public class MenuApp extends AppCompatActivity {
-
-    private final static String TAG = MenuApp.class.getSimpleName();
+public class Collaborators extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_app);
+        setContentView(R.layout.activity_collaborators);
 
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.btn_bg_load));
-            actionBar.setTitle("Dashboard");
+            actionBar.setTitle("Collaborateurs");
         }
-
-    }
-
-    public void displayCollab(View view){
-        Intent intent = new Intent(this, Collaborators.class);
-        startActivity(intent);
     }
 }
